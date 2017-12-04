76810f2f48060618ebb45970322a09d053e42d7f
Blake Sadoway <blakesadoway@gmail.com>
2017-12-04 20:02:44 +0000

    added a callback

---
+++ chunks/chunks-with-callbacks.js
@@ -0,0 +1,38 @@
+var https = require("https");
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  var fullChunkData = "";
+  /* Add your code here */
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+
+      fullChunkData += data;
+      console.log('Chunk Received. Length:', data.length);
+    });
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+    response.on('end', function() {
+      callback(fullChunkData);
+      console.log('Response stream complete.');
+    });
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
