e8fd9761d11a0668caf8e012404bfccc3474e3b3
Blake Sadoway <blakesadoway@gmail.com>
2017-12-04 21:28:27 +0000

    node request exercise

---
+++ http-request/http-request.js
@@ -0,0 +1,19 @@
+var request = require("request");
+var fs = require("fs");
+
+var image = "https://sytantris.github.io/http-examples/future.jpg";
+
+request
+  .get(image)
+  .on('error', function(err) {
+    throw err;
+  })
+  .on('response', function(reponse) {
+    console.log("Downloading image...");
+    console.log(reponse.statusMessage);
+    console.log(reponse.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+  .on('end', function() {
+    console.log("Download complete.");
+  });
